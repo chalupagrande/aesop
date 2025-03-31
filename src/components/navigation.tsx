e8a2fa451ca35b3d4362454a867e1f136b3d89fd
Jamie Skinner <jamie@etcetera.cr>
2025-03-31 09:44:43 -0500

    update opacity

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -1,7 +1,11 @@
+import { useState } from "react";
 import { NavLink } from "react-router";
 import { Button } from "@/components/ui/button";
+import { AlertDialog, AlertDialogContent, AlertDialogTitle, AlertDialogTrigger, AlertDialogDescription } from "./ui/alert-dialog";
 
 export function Navigation() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <>
       <header className="flex flex-row items-center justify-between p-4 z-100">
@@ -9,7 +13,18 @@ export function Navigation() {
           <NavLink to="/"><h1 className="text-3xl font-bold cursor-default">AESOP</h1></NavLink>
           {/* <SignalVideo /> */}
         </div>
-        <a href="mailto:ryanorenstein@gmail.com"><Button variant="link">CONTACT</Button></a>
+        <AlertDialog open={isOpen} onOpenChange={setIsOpen}>
+          <AlertDialogTrigger><Button variant="link">CONTACT</Button></AlertDialogTrigger>
+          <AlertDialogContent>
+            <AlertDialogTitle>Actually maybe don't...</AlertDialogTitle>
+            <AlertDialogDescription>
+              <div className="flex flex-col gap-4 items-start">
+                <p>Listen, we actually don't have much time to get this thing off the ground, and we are working really hard to do that. So if you wouldn't mind, just let us do our thing.</p>
+                <Button onClick={() => setIsOpen(false)}>Close</Button>
+              </div>
+            </AlertDialogDescription>
+          </AlertDialogContent>
+        </AlertDialog>
       </header>
     </>
   )
